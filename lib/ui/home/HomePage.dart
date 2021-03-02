2fecba6284770a84ba4fce7a8e3ce83b972a1402
Guillaume Tartara <gtartara@lamobilery.fr>
2021-03-02 10:41:10 +0100

    chore(Architecture): Setup Dependencies Injection and Bloc architecture

---
+++ lib/ui/home/HomePage.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import 'HomePageBloc.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState(GetIt.I.get<HomePageBloc>());
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final HomePageBloc homePageBloc;
+
+  _HomePageState(this.homePageBloc);
+
+  void _incrementCounter() {
+    homePageBloc.incrementCounter();
+    setState(() {
+      homePageBloc.getCounter();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${homePageBloc.getCounter()}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
